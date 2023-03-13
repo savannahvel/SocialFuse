ea6a4090f7bb5a2223c047cc2801994f60be246f
Savannah Vel <savannah.vel@vivint.com>
2023-03-12 20:26:53 -0700

    corrected db name, created users routes and controllers, added index.js for routes and apis

---
+++ routes/api/usersRoutes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const { getUsers, createUser, getSingleUser, updateUser, deleteUser } = require('../../controllers/userController');
+
+// /api/users
+router.route('/')
+    .get(getUsers)
+    .post(createUser);
+
+// /api/users/:userId
+router.route('/:userId')
+    .get(getSingleUser)
+    .put(updateUser)
+    .delete(deleteUser);
+
+module.exports = router;
